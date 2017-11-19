b08332bf39afea7c9370c59b1c29487892d37de3
mariandaniel24 <mariandaniel2497@gmail.com>
2017-11-19 16:59:10 +0200

    first

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import '../css/style.css';
+
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+
+            <h1> ewfafewwfeawfeaewfa </h1>
+
+            </div>            
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
